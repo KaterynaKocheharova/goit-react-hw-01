ab182e122c188163105741d76e5123454c33486e
Kateryna Kocheharova <kot41901@gmail.com>
2024-04-23 18:18:19 +0300

    base done

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,42 @@
+import PropTypes from "prop-types";
+
+function Profile({ name, tag, location, image, stats }) {
+  return (
+    <div>
+      <div>
+        <img src={image} alt="User avatar" />
+        <p>{name}</p>
+        <p>{tag}</p>
+        <p>{location}</p>
+      </div>
+      <ul>
+        <li>
+          <span>Followers</span>
+          <span>{stats.followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{stats.views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+Profile.propTypes = {
+  name: PropTypes.string.isRequired, // Add this line for name validation
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default Profile;
