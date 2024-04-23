dcd27d370e2e0099d0ddd889d5351c067d403d77
Kateryna Kocheharova <kot41901@gmail.com>
2024-04-23 21:47:11 +0300

    handle userData object

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,6 @@
 import "./App.css";
 import Profile from "./components/Profile/Profile";
-
-const userData = {
-  username: "Jacques Gluke",
-  tag: "jgluke",
-  location: "Ocho Rios, Jamaica",
-  avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
-  stats: {
-    followers: 5603,
-    views: 4827,
-    likes: 1308,
-  },
-};
+import userData from "./data/user-data.json";
 
 export default function App() {
   return (
