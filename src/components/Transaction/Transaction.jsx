25ad56265b06f71785f8ff785e46d138d7783f69
Kateryna Kocheharova <kot41901@gmail.com>
2024-04-27 16:09:07 +0300

    replace switch with clsx

--- src/components/Transaction/Transaction.jsx
+++ src/components/Transaction/Transaction.jsx
@@ -1,22 +1,17 @@
+import clsx from "clsx";
 import css from "./Transaction.module.css";
 
 export default function Transaction({
   transaction: { type, amount, currency },
 }) {
-  let rowClass;
-  switch (type) {
-    case "payment":
-      rowClass = css["payment-row"];
-      break;
-    case "withdrawal":
-      rowClass = css["withdrawal-row"];
-      break;
-    default:
-      rowClass = css["invoice-row"];
-  }
-
   return (
-    <tr className={rowClass}>
+    <tr
+      className={clsx({
+        [css["payment-row"]]: type === "payment",
+        [css["withdrawal-row"]]: type === "withdrawal",
+        [css["invoice-row"]]: type === "invoice",
+      })}
+    >
       <td className={css["table-cell"]}>{type}</td>
       <td className={css["table-cell"]}>{amount}</td>
       <td className={css["table-cell"]}>{currency}</td>
